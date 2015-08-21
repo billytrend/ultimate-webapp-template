dcedf2fba11f001a3f206f5ab4349c396305c14e
billytrend <billy.trend@gmail.com>
2015-08-21 22:12:20 +0100

    Faster compile and missing dependency.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 
 module.exports = {
-    devtool: 'inline-source-map',
+    devtool: 'cheap-eval-source-map',
     entry: [
         'webpack-dev-server/client?http://0.0.0.0:3000',
         'webpack/hot/only-dev-server',
